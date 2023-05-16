e56f8047d1cea35174d060697bfc5f3b8c389eaf
danwen <danwen@penguin>
2023-05-16 11:31:52 -0400

    another one

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -7,33 +7,32 @@ export const App = () => {
 
     const [pokemonpages, setpokemonpages] = useState([]);
 
-    const fetchPages = async() => {
-		try {
+    const fetchPages = async () => {
+        try {
             console.log('fetch activated');
-			const response = await fetch(`${apiURL}/pokemons`);
-			const pokeData = await response.json();
+            const response = await fetch(`${apiURL}/pokemons`);
+            const pokeData = await response.json();
             console.log(pokeData);
-			setpokemonpages(pokeData);
-		} catch (err) {
-			console.log("Oh no an error! ", err)
-		}
-	}
+            setpokemonpages(pokeData);
+        } catch (err) {
+            console.log("Oh no an error! ", err)
+        }
+    }
 
     useEffect(() => {
-		fetchPages();
-	}, []);
+        fetchPages();
+    }, []);
 
     console.log("pokemon store");
     return (
-      <main>
-        <h1>Pokemons Store</h1>
-              <p id="quote"> Welcome to our infamous Pokemon E-Commerce site, there's a ton of options to select from.
-                  We strive to meet the needs of all of our customers and plan to expand our inventory selection in the near future.
-              </p>
-              <PokemonPageList pokemonpages={pokemonpages} setpokemonpages={setpokemonpages}/>
-              <button>Add Pokemon!</button>
+        <main>
+            <h1>Pokemons Store</h1>
+            <p id="quote"> Welcome to our infamous Pokemon E-Commerce site, there's a ton of options to select from.
+                We strive to meet the needs of all of our customers and plan to expand our inventory selection in the near future.
+            </p>
+            <PokemonPageList pokemonpages={pokemonpages} setpokemonpages={setpokemonpages} />
+            <button>Add Pokemon!</button>
 
-      </main> 
+        </main>
     );
-  }
-  
+}
