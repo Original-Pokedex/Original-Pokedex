fea7de7a2a6daf7a94687ec518ff60ac00b70ac6
Ying Xin <chenyingxin92@gmail.com>
2023-05-24 14:20:37 -0400

    moved server.js stuff into server/db/index.js

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,18 @@
-const app = require('./app');
-const { sequelize } = require('./db');
+const { db } = require("./models");
+const app = require("./app");
 
 const { PORT = 3000 } = process.env;
 
-app.listen(PORT, () => {
-    console.log(`Pokemons are ready at http://localhost:${PORT}`);
-});
+const init = async () => {
+    try {
+      await db.sync();
+        
+      app.listen(PORT, () => {
+        console.log(`Server listening at http://localhost:${PORT}`);
+      });
+    } catch (error) {
+      console.error('Error starting server:', error)
+    }
+  };
+  
+  init();
