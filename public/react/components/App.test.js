0b3d31474d98cf110e5d300543b72d6434fa8760
danwen <danwen@penguin>
2023-05-26 12:28:25 -0400

    added test3

--- public/react/components/App.test.js
+++ public/react/components/App.test.js
@@ -1,16 +1,16 @@
 /** @jest-environment jsdom */
 const {fetch} = require('whatwg-fetch');
 global.fetch = fetch;
-// import React from 'react';
+import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import { App } from './App';
 
-// it('should render', () => {
-//   render(<App />);
+it('should render', () => {
+  render(<App />);
 
-//   expect(screen.getByText('Pokedex')).toBeInTheDocument();
-// });
+  expect(screen.getByText('Pokedex')).toBeInTheDocument();
+});
 
 it('should be a working test', () => {
     expect(true).toBe(true);
