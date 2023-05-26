cc2663ec2ad650d3327dc8fb0a1359ad57d74c0d
danwen <danwen@penguin>
2023-05-26 12:23:27 -0400

    added a test2

--- public/react/components/App.test.js
+++ public/react/components/App.test.js
@@ -1,7 +1,7 @@
 /** @jest-environment jsdom */
 const {fetch} = require('whatwg-fetch');
 global.fetch = fetch;
-import React from 'react';
+// import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import { App } from './App';
